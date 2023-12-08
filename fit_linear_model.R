574a494131823c9ee7e82df6c8f7bbd5d997cc1c
pemb6273 <pemb6273@ox.ac.uk>
2023-12-08 00:32:11 +0000

    new commit

--- fit_linear_model.R
+++ fit_linear_model.R
@@ -6,14 +6,15 @@ growth_data <- read.csv("experiment1.csv")
 
 #Case 1. K >> N0, t is small
 
-data_subset1 <- growth_data %>% filter(t<1800) %>% mutate(N_log = log(N))
+data_subset1 <- growth_data %>% filter(t<1000) %>% mutate(N_log = log(N))
 
 model1 <- lm(N_log ~ t, data_subset1)
 summary(model1)
 
 #Case 2. N(t) = K
 
-data_subset2 <- growth_data %>% filter(t>2100)
+data_subset2 <- growth_data %>% filter(t>3000)
 
 model2 <- lm(N ~ 1, data_subset2)
 summary(model2)
+
--- plot_data_and_model.R
+++ plot_data_and_model.R
@@ -10,11 +10,11 @@ logistic_fun <- function(t) {
   
 }
 
-N0 <- 1044.722 #exp(intercept coefficient from model1) bc that is the N value when t~0
+N0 <- 975.54862007 #exp(intercept coefficient from model1 = 6.883) bc that is the N value when t~0
   
-r <- 0.009902 #model1 => coefficient t 
+r <- 0.01004 #model1 => coefficient t 
   
-K <- 5.993e+10 #model2 => estimate intercept bc that is when N stops changing
+K <- 6.000e+10 #model2 => estimate intercept bc that is when N stops changing
 
 ggplot(aes(t,N), data = growth_data) +
   
